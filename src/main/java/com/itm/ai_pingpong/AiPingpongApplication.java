3a2091f585c05591e3b8123575471767d1584847
김찬혁 <cksgur97@gmail.com>
2022-09-21 01:43:46 +0900

    Chore: 연동 MySQL DB
    MySQL:5.7 데이터베이스 연동

---
+++ src/main/java/com/itm/ai_pingpong/AiPingpongApplication.java
@@ -0,0 +1,14 @@
+package com.itm.ai_pingpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPingpongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPingpongApplication.class, args);
+
+	}
+
+}
