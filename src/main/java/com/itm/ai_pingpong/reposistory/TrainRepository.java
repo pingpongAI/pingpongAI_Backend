64a8a6360f8b5b4bd24891ff45e3b800d1bd1f0a
김찬혁 <cksgur97@gmail.com>
2022-09-23 15:29:28 +0900

    Feat: 추가 TrainRepository
    영속성을 가지고 Train을 save 메서드를 통해 테이블에 저장할 수 있다
    유저와 LocalDate를 넘겨받아 특정 날에 훈련한 기록들을 List형태로 반환하는 findTrainSpecificDate 메서드 추가

---
+++ src/main/java/com/itm/ai_pingpong/reposistory/TrainRepository.java
@@ -0,0 +1,31 @@
+import com.itm.ai_pingpong.domain.Train;
+import com.itm.ai_pingpong.domain.User;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class TrainRepository {
+
+  private final EntityManager em;
+
+  public void save(Train train) {
+    em.persist(train);
+  }
+
+  public List<Train> findTrainSpecificDate(User user, LocalDate date) {
+    List<Train> resultList = em.createQuery(
+            "select t from Train t join  t.user u where u.id = :userId and t.startAt >= :startDate and t.startAt < :endDate",
+            Train.class)
+        .setParameter("userId", user.getId())
+        .setParameter("startDate", date.atStartOfDay())
+        .setParameter("endDate", date.atTime(LocalTime.MAX))
+        .getResultList();
+    return resultList;
+  }
+
+}
