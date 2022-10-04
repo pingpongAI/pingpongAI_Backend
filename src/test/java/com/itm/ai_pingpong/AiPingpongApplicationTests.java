6db9698a82c636ad5de3712283ab11b20647663c
김찬혁 <cksgur97@gmail.com>
2022-10-04 16:27:44 +0900

    Fix: 제거 @SpringBootTest

--- src/test/java/com/itm/ai_pingpong/AiPingpongApplicationTests.java
+++ src/test/java/com/itm/ai_pingpong/AiPingpongApplicationTests.java
@@ -1,10 +1,8 @@
 package com.itm.ai_pingpong;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootTest
 @EnableJpaAuditing
 class AiPingpongApplicationTests {
 
