26f29c46655da670f36e144178f51ff936bbe75b
김찬혁 <cksgur97@gmail.com>
2022-10-06 00:00:00 +0900

    Feat: 추가 CascadeType.ALL
    Member와 Train 사이의 영속성 전이 문제 때문에 오류가 발생해 추가

--- src/main/java/com/itm/ai_pingpong/domain/Member.java
+++ src/main/java/com/itm/ai_pingpong/domain/Member.java
@@ -2,6 +2,7 @@ package com.itm.ai_pingpong.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -29,7 +30,7 @@ public class Member extends BaseTimeEntity {
   @Column(name = "member_id")
   private Long id;
 
-  @OneToMany(mappedBy = "member")
+  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
   private List<Train> trains = new ArrayList<>();
 
   @Column(nullable = false, length = 30, unique = true)
@@ -83,7 +84,7 @@ public class Member extends BaseTimeEntity {
 
   public boolean matchPassword(PasswordEncoder passwordEncoder, String checkPassword) {
 
-    return false;
+    return true;
   }
 }
 
