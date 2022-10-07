ed80e3030d68aa977f10e30179fffe78a7c191a8
Chanhook <cksgur97@gmail.com>
2022-10-07 12:26:56 +0900

    Feat: 추가 saveTrain 메서드
    훈련내용을 디비에 저장하는 함수 추가

--- src/main/java/com/itm/ai_pingpong/service/TrainService.java
+++ src/main/java/com/itm/ai_pingpong/service/TrainService.java
@@ -1,7 +1,9 @@
 package com.itm.ai_pingpong.service;
 
 import com.itm.ai_pingpong.domain.Member;
+import com.itm.ai_pingpong.domain.Train;
 import com.itm.ai_pingpong.dto.TrainResponseDto;
+import com.itm.ai_pingpong.dto.TrainSaveDto;
 import com.itm.ai_pingpong.reposistory.MemberRepository;
 import com.itm.ai_pingpong.reposistory.TrainRepository;
 import java.util.Optional;
@@ -20,51 +22,51 @@ public class TrainService {
   private final TrainRepository trainRepository;
 
   /*
-    테스트
-    private final PasswordEncoder passwordEncoder;
+  //    테스트
+      private final PasswordEncoder passwordEncoder;
 
-    @PostConstruct
-    public void init() {
-      Member member = memberRepository.save(getMember());
-      Member member2 = memberRepository.save(getMember2());
-      for (int i = 1; i <= 25; ++i) {
-        trainRepository.save(Train.builder().member(member)
-            .type(TrainType.FOREHAND)
-            .wrongCount(i)
-            .rightCount(25 - i)
-            .build());
-        trainRepository.save(Train.builder().member(member2)
-            .type(TrainType.FOREHAND)
-            .wrongCount(i)
-            .rightCount(25 - i)
-            .build());
+      @PostConstruct
+      public void init() {
+        Member member = memberRepository.save(getMember());
+        Member member2 = memberRepository.save(getMember2());
+        for (int i = 1; i <= 25; ++i) {
+          trainRepository.save(Train.builder().member(member)
+              .type(TrainType.FOREHAND)
+              .wrongCount(i)
+              .rightCount(25 - i)
+              .build());
+          trainRepository.save(Train.builder().member(member2)
+              .type(TrainType.FOREHAND)
+              .wrongCount(i)
+              .rightCount(25 - i)
+              .build());
+        }
       }
-    }
 
-    private Member getMember() {
-      Member member = Member.builder()
-          .email("kim@naver.com")
-          .password("password")
-          .tel("010-1234-1234")
-          .name("kim").
-          status(MemberStatus.BASIC)
-          .build();
-      member.encodePassword(passwordEncoder);
-      return member;
-    }
+      private Member getMember() {
+        Member member = Member.builder()
+            .email("kim@naver.com")
+            .password("password")
+            .tel("010-1234-1234")
+            .name("kim").
+            status(MemberStatus.BASIC)
+            .build();
+        member.encodePassword(passwordEncoder);
+        return member;
+      }
 
 
-    private Member getMember2() {
-      Member member = Member.builder()
-          .email("kim2@naver.com")
-          .password("password")
-          .tel("010-1234-1234")
-          .name("kim").
-          status(MemberStatus.BASIC)
-          .build();
-      member.encodePassword(passwordEncoder);
-      return member;
-    }
+      private Member getMember2() {
+        Member member = Member.builder()
+            .email("kim2@naver.com")
+            .password("password")
+            .tel("010-1234-1234")
+            .name("kim").
+            status(MemberStatus.BASIC)
+            .build();
+        member.encodePassword(passwordEncoder);
+        return member;
+      }
   */
   public Page<TrainResponseDto> findAll(long id, Pageable pageable) {
     Optional<Member> findMember = memberRepository.findById(id);
@@ -72,4 +74,11 @@ public class TrainService {
         .map(TrainResponseDto::from);
   }
 
+  public TrainSaveDto saveTrain(long id, TrainSaveDto trainSaveDto) {
+    Optional<Member> findMember = memberRepository.findById(id);
+    Train train = trainSaveDto.toEntity();
+    train.updateMember(findMember.orElse(null));
+    trainRepository.save(train);
+    return trainSaveDto;
+  }
 }
